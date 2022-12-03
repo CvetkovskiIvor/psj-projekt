b50eef6fe8dd6a2875b5391da9bd2f3b8c10dcc6
David <david.katalinic9@gmail.com>
2022-12-03 16:39:41 +0100

    Učitavanje dataset-a i osnovna vizualizacija podataka

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from pandas import read_csv
+
+
+
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+data = read_csv('C:\\Users\\User\\Documents\\Fakultet\\Programiranje skriptni jezici\\psj-projekt\\Dataset\\housing.csv', header=None, delimiter=r"\s+", names=column_names)
+
+#Provjera da li postoje redovi bez vrijednosti
+print(data.isnull().sum())
+
+print(data.describe())
+
+#Prikaz korelacije između pojedinih varijabli sustava
+correlation = data.corr().abs()
+plt.figure(figsize=(20, 10))
+sns.heatmap(correlation, annot=True)
+plt.show()
