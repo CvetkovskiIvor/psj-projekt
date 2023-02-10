c13efbe3c55bc86c20a7c590de1bb6cb7627742f
Owner <korlevice@gmail.com>
2023-02-10 22:14:53 +0100

    Implementirano vrijeme izvršavanja i prikaz podataka SVR-a

---
+++ SVR.py
@@ -0,0 +1,141 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+
+from pandas import read_csv
+
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+data = read_csv(
+    'C:\\Users\\Owner\\Desktop\\Skriptni\\psj-projekt\\Dataset\\housing.csv',
+    header=None, delimiter=r"\s+", names=column_names)
+
+# Provjera da li postoje redovi bez vrijednosti
+print(data.isnull().sum())
+
+print(data.describe())
+
+# Prikaz korelacije između pojedinih varijabli sustava
+correlation = data.corr().abs()
+plt.figure(figsize=(20, 10))
+sns.heatmap(correlation, annot=True)
+plt.show()
+
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn import preprocessing
+#Skalira  vrijednosti u jedan jedinstveni range
+min_max_scaler = preprocessing.MinMaxScaler()
+#Odabrani su stupci koji imaju najveću korelaciju sa traženim stupcom
+column_corr = ['LSTAT', 'INDUS', 'NOX', 'PTRATIO', 'RM', 'TAX','AGE','DIS']
+#X = data.drop(['MEDV'], axis=1)
+X = data.loc[:,column_corr]
+X = min_max_scaler.fit_transform(X)
+# Tražena varijabla/stupac
+y = data['MEDV']
+
+# Podjela dataset-a na dva dijela. Jedan za trening, a jedan za testiranje
+# Veličina dataset-a za test je 30% orginalne veličine testa
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=25)
+
+print("Shape of X_train: ",X_train.shape)
+print("Shape of X_test: ", X_test.shape)
+print("Shape of y_train: ",y_train.shape)
+print("Shape of y_test",y_test.shape)
+
+
+#Support Vector Regression
+print('Support Vector Regrresion:')
+from sklearn.svm import SVR
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import cross_val_score
+from scipy.stats import expon, reciprocal
+
+n_estimators = [int(x) for x in np.arange(start = 10, stop = 2000, step = 10)]
+max_features = [0.5,'auto', 'sqrt','log2']
+min_samples_leaf = [1, 2, 4]
+bootstrap = [True, False]
+param_distribs = {
+        'kernel': ['linear', 'rbf','poly','sigmoid'],
+        'C': reciprocal(20, 200000),
+        'gamma': expon(scale=1.0),
+        'degree': expon(scale=1.0),
+    }
+
+#SVRegr =SVR(n_estimators=500, min_samples_leaf=1, max_features=0.5,bootstrap=False)
+SVRegr = SVR(C=211.49654965532167, epsilon=0.1, degree=0.04127375231664331, gamma=1.2401627989937203)
+#SVRegr = SVR()
+#SVRegr_random = RandomizedSearchCV(estimator=SVRegr, param_distributions = param_distribs, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+
+#SVRegr_random.fit(X_train, y_train)
+#print(SVRegr_random.best_params_)
+
+#timer start
+t1 = time.time()
+
+# Training data
+scores_map = {}
+scores = cross_val_score(SVRegr, X_train, y_train, cv=10)
+scores_map['Support Vector Regression'] = scores
+print("Mean score of %0.2f with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+SVRegr.fit(X_train, y_train)
+#print(SVRegr.get_params(deep=True))
+
+train_predicted = SVRegr.predict(X_train)
+print("Predicted MEDV: ", train_predicted)
+# Provjera ispravnosti algoritma
+print("Accuracy of Support Vector algorithm ", SVRegr.score(X_train,y_train))
+# Računa koliko varijacije u dobivenom rezultatu se može predvidjeti na temelju ulazne varijable
+# Što je broj bliže 1 to je algoritam točniji
+print('R^2:',metrics.r2_score(y_train, train_predicted))
+print('Adjusted R^2:',1 - (1-metrics.r2_score(y_train, train_predicted))*(len(y_train)-1)/(len(y_train)-X_train.shape[1]-1))
+# Prosjek kvadrata razlike između dobivenih vrijednosti i stvarnih vrijednosti
+print('MAE:',metrics.mean_absolute_error(y_train, train_predicted))
+# Što je manji MSE to je greška manja
+#https://datagy.io/mean-squared-error-python/ - dodatno objašnjenje
+print('MSE:',metrics.mean_squared_error(y_train, train_predicted))
+# https://www.kaggle.com/general/215997 - Objašnjenje za RMSE grešku
+print('RMSE:',np.sqrt(metrics.mean_squared_error(y_train, train_predicted)))
+
+plt.scatter(y_train, train_predicted)
+plt.xlabel("MEDV")
+plt.ylabel("Predicted MEDV")
+plt.title("Support Vector Training data: MEDV vs Predicted MEDV")
+plt.show()
+
+# Test data
+print("---------------------------------------------------")
+#SupportVec.fit(X_test,y_test)
+test_predicted = SVRegr.predict(X_test)
+
+#kraj timera
+t2 = time.time()
+
+print("Accuracy of Support Vector algorithm for test Data ", SVRegr.score(X_train,y_train))
+print("Time of execution: ", t2 - t1,"secs")
+print('R^2 Test:',metrics.r2_score(y_test, test_predicted))
+print('Adjusted R^2:', 1 - (1-metrics.r2_score(y_test, test_predicted))*(len(y_test)-1)/(len(y_test)-X_test.shape[1]-1))
+print('MAE Test:',metrics.mean_absolute_error(y_test, test_predicted))
+print('MSE Test:',metrics.mean_squared_error(y_test, test_predicted))
+print('RMSE Test:',np.sqrt(metrics.mean_squared_error(y_test, test_predicted)))
+
+plt.scatter(y_test, test_predicted)
+plt.xlabel("MEDV")
+plt.ylabel("Predicted MEDV")
+plt.title("Support Vector Test data: MEDV vs Predicted MEDV")
+plt.show()
+
+plt.scatter(test_predicted,y_test-test_predicted)
+plt.title("Support Vector Reg Predicted vs residuals")
+plt.xlabel("Predicted")
+plt.ylabel("Residuals")
+plt.show()
+
+print('------------------------------------------------------')
+
+plt.figure(figsize=(20, 10))
+scores_map = pd.DataFrame(scores_map)
+sns.boxplot(data=scores_map)
+plt.show()
